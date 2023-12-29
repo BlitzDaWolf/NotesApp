f83f16df69dcff7c2532fc196eead61199be9076
BlitzDaWolf <botjes2@hotmail.com>
2023-12-29 02:26:14 +0100

    Added User Identity

---
+++ Database/Context/NotesContext.cs
@@ -0,0 +1,13 @@
+﻿using Database.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Context
+{
+    public class NotesContext : IdentityDbContext<User>
+    {
+        public NotesContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- NotesApp.API/Program.cs
+++ NotesApp.API/Program.cs
@@ -1,3 +1,8 @@
+using Database.Context;
+using Database.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +12,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorizationBuilder();
+
+builder.Services.AddDbContext<NotesContext>(x => x.UseSqlite("DataSource=app.db", b => b.MigrationsAssembly("NotesApp.API")));
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<NotesContext>()
+    .AddApiEndpoints();
+
 var app = builder.Build();
+
+app.MapIdentityApi<User>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,9 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
